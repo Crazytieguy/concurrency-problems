98676568156cce0ce3da78ef93ace333091ac379
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-07-15 00:59:49 +0300

    add ride mechanic

--- uber_ride_problem.py
+++ uber_ride_problem.py
@@ -2,7 +2,8 @@ import random
 import threading
 import time
 from collections import deque
-from threading import Event, Lock, Thread
+from queue import Queue
+from threading import Barrier, Lock, Thread
 
 START = time.time()
 
@@ -16,9 +17,36 @@ def log(message: str):
         print(f"{now:.3f} {thread_name}: {message}")
 
 
+class Ride:
+    barrier: Barrier
+    lock: Lock
+    driver_chosen: bool
+    passengers: list[str]
+
+    def __init__(self):
+        self.barrier = Barrier(4)
+        self.lock = Lock()
+        self.driver_chosen = False
+        self.passengers = []
+
+    def drive(self):
+        log(f"driving {self.passengers}")
+
+    def take_seat(self) -> bool:
+        self.passengers.append(threading.current_thread().name)
+        log("seated")
+        self.barrier.wait()
+        with self.lock:
+            if self.driver_chosen:
+                return False
+            else:
+                self.driver_chosen = True
+                return True
+
+
 class Uber:
-    democrats: deque[Event]
-    republicans: deque[Event]
+    democrats: deque[Queue]
+    republicans: deque[Queue]
     lock: Lock
 
     def __init__(self):
@@ -26,55 +54,63 @@ class Uber:
         self.republicans = deque()
         self.lock = Lock()
 
-    def order_uber_democrat(self):
-        log("democrat waiting")
-        event = Event()
+    def order_uber_democrat(self) -> Ride:
+        log("waiting")
+        queue = Queue(1)
         with self.lock:
-            self.democrats.append(event)
+            self.democrats.append(queue)
             self.try_find_ride()
-        event.wait()
-        log("democrat seated")
+        return queue.get()
 
-    def order_uber_republican(self):
-        log("replublican waiting")
-        event = Event()
+    def order_uber_republican(self) -> Ride:
+        log("waiting")
+        queue = Queue(1)
         with self.lock:
-            self.republicans.append(event)
+            self.republicans.append(queue)
             self.try_find_ride()
-        event.wait()
-        log("replublican seated")
+        return queue.get()
 
     def try_find_ride(self):
         if len(self.democrats) >= 2 and len(self.republicans) >= 2:
+            ride = Ride()
             for _ in range(2):
-                self.democrats.popleft().set()
+                self.democrats.popleft().put(ride)
             for _ in range(2):
-                self.republicans.popleft().set()
+                self.republicans.popleft().put(ride)
             return
         if len(self.democrats) >= 4:
+            ride = Ride()
             for _ in range(4):
-                self.democrats.popleft().set()
+                self.democrats.popleft().put(ride)
             return
         if len(self.republicans) >= 4:
+            ride = Ride()
             for _ in range(4):
-                self.republicans.popleft().set()
+                self.republicans.popleft().put(ride)
             return
 
 
-def random_delay(callback):
-    def inner():
-        time.sleep(random.random() * 10)
-        callback()
+def democrat(uber: Uber):
+    time.sleep(random.random() * 10)
+    ride = uber.order_uber_democrat()
+    if ride.take_seat():
+        ride.drive()
 
-    return inner
+
+def republican(uber: Uber):
+    time.sleep(random.random() * 10)
+    ride = uber.order_uber_republican()
+    time.sleep(random.random())
+    if ride.take_seat():
+        ride.drive()
 
 
 def main():
     uber = Uber()
 
     threads = [
-        Thread(target=random_delay(uber.order_uber_democrat)) for _ in range(10)
-    ] + [Thread(target=random_delay(uber.order_uber_republican)) for _ in range(10)]
+        Thread(target=democrat, args=(uber,), name=f"Democ-{i}") for i in range(10)
+    ] + [Thread(target=republican, args=(uber,), name=f"Repub-{i}") for i in range(10)]
 
     for t in threads:
         t.start()
